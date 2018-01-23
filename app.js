e3ae44d1e79a1e57f594598404815b35cb96cfd5
Glen Fannin <devglen@icloud.com>
2018-01-22 21:50:45 -0500

    Fixed issues, added new logic

--- app.js
+++ app.js
@@ -23,79 +23,187 @@ document.addEventListener( "DOMContentLoaded", () => {
         console.log('url', theUrl);
     
         win.loadURL(theUrl);
+        win.webContents.openDevTools();
     });
-
-    
-
-} );
-
-document.getElementById("saveMachine").addEventListener("click", function(){
-    console.log("this should close!");
-
-    var dataToSend = document.querySelector("form").serialize();
-
-    var filename = document.getElementById("machineName").value.split(" ").join("_");
-    jetpack.write("./machines/" + filename + ".json", dataToSend);
-    alert("Form data: " + dataToSend);
-    remote.getCurrentWindow().getParentWindow().reload();
-    var window = remote.getCurrentWindow();
-    window.close();
-});
-
-document.getElementById("newParameter").addEventListener("click", function(){
-    var paramContainer = document.getElementById("machineParameters");
-    var paramCount = paramContainer.childElementCount;
-
-    var paramKey = document.createElement("label");
-    paramKey.setAttribute("for", "key_val_"+paramCount);
-    paramKey.innerText = "Key: ";
-
-    var paramVal = document.createElement("input");
-    paramVal.setAttribute("type", "text");
-    paramVal.setAttribute("name", "key_val_"+paramCount);
-
-    var paramKey2 = document.createElement("label");
-    paramKey2.setAttribute("for", "val_val_"+paramCount);
-    paramKey2.innerText = "Value: ";
-
-    var paramVal2 = document.createElement("input");
-    paramVal2.setAttribute("type", "text");
-    paramVal2.setAttribute("name", "val_val_"+paramCount);
-
-    var lb = document.createElement("br");
-    var container = document.createElement("div");
-    container.appendChild(lb);
-
-    container.appendChild(paramKey);
-    container.appendChild(paramVal);
-
-    container.appendChild(lb);
-
-    container.appendChild(paramKey2);
-    container.appendChild(paramVal2);
-
-    paramContainer.appendChild(container);
 
 });
 
 function getMachines() {
-    var machineFiles = jetpack.list("./machines/");
-    console.log(machineFiles);
+    try {
+        var machinesContainer = document.getElementById("machines");
+        var machineFiles = jetpack.list("./machines/");
+        console.log(machineFiles);
+    
+        for (var i = 0; i < machineFiles.length; i++) {
+            var machineFile = jetpack.read("./machines/" + machineFiles[i], "json");
+            
+            var header = document.createElement("h1");
+            header.innerText = machineFile.name;
+
+            var machineFileName = document.createElement("input");
+            machineFileName.setAttribute("type", "hidden");
+            machineFileName.setAttribute("id", "machine_filename_" + i);
+            machineFileName.setAttribute("value", machineFiles[i]);
+
+            var machineContainer = document.createElement("div");
+            machineContainer.appendChild(header);
+            machineContainer.appendChild(machineFileName);
+
+            var inputCounter = 0;
+            while (machineFile["input_title_" + inputCounter]) {
+                var inputTitleLabel = document.createElement("label");
+                inputTitleLabel.setAttribute("for", machineFile["input_title_" + inputCounter] + "_val");
+                inputTitleLabel.innerText = machineFile["input_title_" + inputCounter];
+
+                var inputValue = document.createElement("input");
+                inputValue.setAttribute("type", "text");
+                inputValue.setAttribute("name", machineFile["input_title_" + inputCounter] + "_val");
+                inputValue.setAttribute("id", machineFile["input_title_" + inputCounter] + "_val");
+
+                var paragraph = document.createElement("p");
+                paragraph.appendChild(inputTitleLabel);
+                paragraph.appendChild(inputValue);
+                machineContainer.appendChild(paragraph);
+
+                inputCounter++;
+            }
+            getOperationsForMachine(machineContainer, machineFile.name);
+
+            machineContainer.appendChild(document.createElement("br"));
+
+            var operationBtn = document.createElement("button");
+            operationBtn.setAttribute("id", "newOperation+" + machineFile.name.split(" ").join("_"));
+            operationBtn.setAttribute("class", "newOp");
+            operationBtn.innerText = "Add Operation";
+            
+            machineContainer.appendChild(operationBtn);
+
+            var generateBtn = document.createElement("button");
+            generateBtn.setAttribute("id", "generateProgCode+" + machineFile.name.split(" ").join("_"));
+            generateBtn.setAttribute("class", "genProgCode");
+            generateBtn.innerText = "Generate Code";
+
+            machineContainer.appendChild(generateBtn);
+
+            machinesContainer.appendChild(machineContainer);
+
+            machinesContainer.appendChild(document.createElement("hr"));
+
+            console.log(machineFile);
+        }
+        initOperationBtnListeners();
+        initCodeGenBtnListeners();
+    } catch (e) {
+        console.log("No Machines saved.");
+    }
 }
 
-HTMLElement.prototype.serialize = function(){
-    var obj = {};
-    var elements = this.querySelectorAll( "input, select, textarea" );
-    for( var i = 0; i < elements.length; ++i ) {
-        var element = elements[i];
-        var name = element.name;
-        var value = element.value;
+function initOperationBtnListeners() {
+    var classname = document.getElementsByClassName("newOp");
 
-        if( name ) {
-            obj[ name ] = value;
-        }
+    var newOpFunc = function() {
+        var idVal = this.getAttribute("id");
+        var machineName = idVal.split("+")[1];
+
+        let win = new remote.BrowserWindow({
+            parent: remote.getCurrentWindow(),
+            modal: true
+        });
+    
+        var theUrl = 'file://' + __dirname + '/add-operation.html?machine=' + machineName;
+    
+        win.loadURL(theUrl);
+        win.webContents.openDevTools();
+    };
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', newOpFunc, false);
     }
-    return JSON.stringify( obj );
+}
+
+function initCodeGenBtnListeners() {
+    var classname = document.getElementsByClassName("genProgCode");
+
+    var genCodeFunc = function() {
+        var idVal = this.getAttribute("id");
+        
+        alert("TODO: WIP - Make this generate the code! ID val: " + idVal );
+        //win.webContents.openDevTools();
+    };
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', genCodeFunc, false);
+    }
+}
+
+function getOperationsForMachine(container, machineName) {
+    try {
+        var opsContainer = document.createElement("div");
+        var operationFiles = jetpack.list("./operations/");
+        console.log("operation files: " , operationFiles);
+
+        if (!operationFiles) {
+            container.appendChild(document.createElement("span"));
+            return;
+        }
+
+        for (var i = 0; i < operationFiles.length; i++) {
+            var formattedMachineName = machineName.split(" ").join("_");
+            
+            if (operationFiles[i].indexOf(formattedMachineName) < 0) {
+                container.appendChild(opsContainer);
+                continue;
+            }
+
+            var operationFile = jetpack.read("./operations/" + operationFiles[i], "json");
+
+            var opContainer = document.createElement("div");
+            opContainer.setAttribute("id", operationFile.name);
+
+            var header = document.createElement("h5");
+            header.innerText = operationFile.name;
+
+            opContainer.appendChild(header);
+
+            var opFileInfo = document.createElement("input");
+            opFileInfo.setAttribute("type", "hidden");
+            opFileInfo.setAttribute("value", operationFiles[i]);
+            opFileInfo.setAttribute("id", "opfile_" + operationFiles[i] + "_" + i);
+
+            opContainer.appendChild(opFileInfo);
+
+            var inputCounter = 0;
+            while (operationFile["input_title_" + inputCounter]) {
+                var inputTitleLabel = document.createElement("label");
+                inputTitleLabel.setAttribute("for", operationFile["input_title_" + inputCounter] + "_val");
+                inputTitleLabel.innerText = operationFile["input_title_" + inputCounter];
+
+                var inputValue = document.createElement("input");
+                inputValue.setAttribute("type", "text");
+                inputValue.setAttribute("name", operationFile["input_title_" + inputCounter] + "_val");
+                inputValue.setAttribute("id", operationFile["input_title_" + inputCounter] + "_val");
+
+                var paragraph = document.createElement("p");
+                paragraph.appendChild(inputTitleLabel);
+                paragraph.appendChild(inputValue);
+                opContainer.appendChild(paragraph);
+
+                inputCounter++;
+            }
+
+            if (inputCounter === 0) {
+                var msgSpan = document.createElement("span");
+                msgSpan.innerHTML = "<em>No inputs defined for this operation.</em>";
+                opContainer.appendChild(msgSpan);
+            }
+
+            opsContainer.appendChild(opContainer);
+        }
+
+        container.appendChild(opsContainer);
+    } catch (e) {
+        console.log("ERROR!!!!", e);
+    }
 }
 
 ipcRenderer.on( "dialog-response", ( e, arg ) => {
